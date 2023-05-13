f33cbe052b2de1279e70a79a71ff2818fd08ad7e
njain204 <105885948+njain204@users.noreply.github.com>
2023-05-13 22:29:42 +0530

    Time: 3 ms (87.94%), Space: 9.6 MB (12.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,8 +4,6 @@ public:
 {
     string result;
     int n1 = str1.length(), n2 = str2.length();
- 
-    // Compare str1 and str2
     for (int i=0, j=0; i<=n1-1&&j<=n2-1; i++,j++)
     {
         if (str1[i] != str2[j])
