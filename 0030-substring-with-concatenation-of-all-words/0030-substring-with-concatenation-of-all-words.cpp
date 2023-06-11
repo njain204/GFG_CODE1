e731ba6fa863a13740d11b750808065d1cca5242
njain204 <105885948+njain204@users.noreply.github.com>
2023-06-11 13:09:02 +0530

    Time: 1148 ms (13.05%), Space: 302.5 MB (28.36%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> res;
-        if (words.empty()) return res;
-        int n = s.size(), m = words.size(), k = words[0].size();
-        unordered_map<string, int> mp;
-        for (string& word : words) mp[word]++;
-        for (int i = 0; i < k; i++) {
-            int left = i, count = 0;
-            unordered_map<string, int> t;
-            for (int j = i; j <= n - k; j += k) {
-                string str = s.substr(j, k);
-                if (mp.count(str)) {
-                    t[str]++;
-                    if (t[str] <= mp[str]) count++;
-                    else {
-                        while (t[str] > mp[str]) {
-                            string str1 = s.substr(left, k);
-                            t[str1]--;
-                            if (t[str1] < mp[str1]) count--;
-                            left += k;
-                        }
+    vector<int>ans;
+        int n=words.size();
+        int m=words[0].size();
+        int total=n*m;
+        if(s.size()<total){
+            return ans;
+        }
+        for(int i=0;i<=s.size()-total;i++){
+            unordered_map<string,int>mp;
+            for(int j=0;j<words.size();j++){
+                mp[words[j]]++;
+            }
+            int k;
+            for(k=0;k<n;k++){   
+                string temp=s.substr(i+k*m,m);
+                if(mp.count(temp)==0){
+                    break;
+                }
+                else{
+                    if(mp[temp]!=0){
+                        mp[temp]--;
                     }
-                    if (count == m) {
-                        res.push_back(left);
-                        t[s.substr(left, k)]--;
-                        count--;
-                        left += k;
+                    else{
+                        break;
                     }
-                } else {
-                    t.clear();
-                    count = 0;
-                    left = j + k;
                 }
             }
+            if(k==n){
+                ans.push_back(i);
+            }
         }
-        return res;
+        return ans ;
     }
 };
