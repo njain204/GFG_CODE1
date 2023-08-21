6409c498c858a4e6d427fc2e347794a83befd5d5
Neerav Jain <105885948+njain204@users.noreply.github.com>
2023-08-21 22:29:20 +0530

    Time: 12 ms (77.76%), Space: 25.4 MB (33.07%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    unordered_map<TreeNode *, int> map;
+    int rob(TreeNode* root) {
+        traverse(root);
+        return map[root];
+    }
+    void traverse(TreeNode* root){
+        if(root == nullptr) return;
+        if(root->left == nullptr && root->right == nullptr){
+            map[root] = root->val;
+            return;
+        }
+        int countRoot = 0;
+        int xRoot = 0;
+        if(root->left != nullptr && !map.count(root->left)){
+            traverse(root->left);
+            xRoot += map[root->left];
+            if(root->left->left!=nullptr) countRoot += map[root->left->left];
+            if(root->left->right!=nullptr) countRoot += map[root->left->right];
+        }
+        if(root->right != nullptr && !map.count(root->right)){
+            traverse(root->right);
+            xRoot += map[root->right];
+            if(root->right->left!=nullptr) countRoot += map[root->right->left];
+            if(root->right->right!=nullptr) countRoot += map[root->right->right];
+        }
+        map[root] = max(xRoot, countRoot+root->val);
+    }
+};
