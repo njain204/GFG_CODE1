97dc3c64c3250d9b44a87fb0de04bcd6440f1b28
Neerav Jain <105885948+njain204@users.noreply.github.com>
2023-08-31 15:08:43 +0530

    Time: 22 ms (84.39%), Space: 58 MB (24.18%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -6,19 +6,15 @@ public:
 		inorder(root);
 		swap(firstMistake->val, secondMistake->val);
 	}
-
 	void inorder(TreeNode* root) {
 		if(root == nullptr) 
 			return;
-
 		inorder(root->left);
-
 		if(firstMistake == nullptr && root->val < pre->val)
 			firstMistake = pre;
 		if(firstMistake != nullptr && root->val < pre->val)
 			secondMistake = root;
 		pre = root;
-
 		inorder(root->right);
 	}
 };
