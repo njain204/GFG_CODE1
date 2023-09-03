89d45bc09eaf510cf523a5bbb249f30c58a3d9a0
Neerav Jain <105885948+njain204@users.noreply.github.com>
2023-09-04 01:29:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.90%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+       if(dividend == -2147483648 && divisor == -1) return 2147483647;
+       // int div = dividend/divisor;
+        //return div;
+          
+        return dividend/divisor;
+    }
+};
